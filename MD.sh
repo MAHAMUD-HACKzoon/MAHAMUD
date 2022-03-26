1858b3a51f9851f7e9fd3a1b0bec893dc53e206b
MD Mahamud Hasan <102261379+MAHAMUD-HACKzoon@users.noreply.github.com>
2022-03-27 00:44:54 +0600

    Create MD.sh

---
+++ MD.sh
@@ -0,0 +1,31 @@
+clear
+echo
+echo "
+   < ━━━━━━━━━ [★] C L A Y - H A C K E R [★] ━━━━━━━━━━━━ >  " |lolcat
+echo
+    echo "  We Are AnonyMouse" |lolcat
+
+
+figlet    '    CLAY' |lolcat
+
+echo "
+             We Do Not HaCk to impress
+                        We HaCk To Express
+                    Clowns Laughing At You 😈😈
+   < 😈━━━━━━━━━━━ [★]MD Mahamud Hasan [★] ━━━━━━━━━━━━😈 > " |lolcat
+
+
+python /data/data/com.termux/files/usr/etc/wlc.py
+if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
+        command_not_found_handle() {
+                /data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+        }
+fi
+
+#PS1='\033[1;31mClay~#'
+
+PS1='\[\e[1;34m┌──\a─T─I─M─E─\a──┐\033[1;34m\a┌──\a─D─A─T─E─\a───>\033[1;34m
+\a┌─[\033[1;93m \@\033[1;34m ]──[\033[1;93m \d\033[1;34m ]\033[1;34m
+\a├─[\033[1;32m\w\033[1;34m]\033[1;34m
+\[\e[34m\]└─>\[\e[35m\]THBD\[\e[34m\][~]:#\[\e[1;32m\] '
+echo -e '\e[6 q'
